2d253a70b0f74ce6f301fe1f9754099dfb651da2
Deepanshu Negi <deepanshu9760873328@gmail.com>
2025-01-16 19:37:06 +0530

    initialise

---
+++ chaicode/c.js
@@ -0,0 +1,31 @@
+
+// data typees 
+console.log(45);
+//alert(3+3); //  this will show error as we are running code in nodejs not browser it does not contain engine support 
+//The error occurs because document.write() is part of the browser environment's DOM (Document Object Model) and is not available in Node.js.
+//  Node.js is a server-side JavaScript runtime that does not include a browser environment or the document object.
+// document.write (45); 
+
+let name = "hello";
+let age = 24;
+let isloggedin = false ;
+
+ // data types in  the javascript 
+// number =>2 to power 53
+//bigint
+//string => " "
+//boolean=> false or true
+//null => stand alone value 
+// undefined => not assigned 
+// symbol => unique 
+
+
+
+// object  
+
+console.log(typeof"helo");
+console.log(typeof null); // here the type of null is object 
+console.log (typeof undefined); // type if undifined 
+
+
+// 
---
+++ chaicode/c2.js
@@ -0,0 +1,18 @@
+// CONVERSIONS 
+
+
+let score = "hello 34";
+console.log(typeof (score));
+
+let valueinumber = Number (score);
+let oolean = true ;
+let boolean = false ;
+console.log(valueinumber); // will give nan (not a number)
+console.log(typeof (valueinumber));
+console.log(typeof (oolean )); // will provide you boolean
+console.log(typeof (boolean));
+
+console.log (Number(oolean)); // true means  1 
+console.log(Number(boolean));// false means 0 
+console.log (String(45)); 
+console.log(Boolean(45)); // provides booleann 

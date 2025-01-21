c4e9afba3d3d36394126faa188551a2bbae1270f
Deepanshu Negi <deepanshu9760873328@gmail.com>
2025-01-22 00:42:55 +0530

    urgent

---
+++ chaicode/strings.js
@@ -0,0 +1,8 @@
+const name = "hello ";
+const repocount = 45;
+console.log (name+repocount); // here  if we write name it is (depracated) it means particular function library is not recommended to use .
+console.log (name , repocount); 
+
+
+// string interpolation 
+

ef27cf02a50cdcebfc1d14f98d7d0ad62f5a9026
Deepanshu Negi <deepanshu9760873328@gmail.com>
2025-01-29 23:54:29 +0530

    definitions

--- chaicode/array.js
+++ chaicode/array.js
@@ -1,3 +1,31 @@
 /* ARRAYS IN JAVASCRIPT */
 /* ARRAY IS A DATA STRUCTURE IN JAVASCRIPT */
 
+/* In JavaScript, a primitive 
+(primitive value, primitive data type) is data that is not an object and
+ has no methods or properties. There are 7 primitive data types:
+
+string
+number
+bigint
+boolean
+undefined
+symbol
+null
+
+*/
+
+/*
+JavaScript arrays are resizable and can contain a mix of different data types. (When those characteristics are undesirable, use typed arrays instead.)
+JavaScript arrays are not associative arrays and so, array elements cannot be accessed using arbitrary strings as indexes, but must be accessed using nonnegative integers (or their respective string form) as indexes.
+JavaScript arrays are zero-indexed: the first element of an array is at index 0, the second is at index 1, and so on — and the last element is at the value of the array's length property minus 1.
+JavaScript array-copy operations create shallow copies. (All standard built-in copy operations with any JavaScript objects create shallow copies, rather than deep copies).
+*/
+
+// arrays are stored inside the square brackets//
+const myarr = [0,"hekk" , 24,3533,4,34,true];
+console.log(myarr[3]);
+
+// arrays in js create shallow copies .
+
+

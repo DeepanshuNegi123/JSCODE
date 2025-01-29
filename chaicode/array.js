93e4fa86fa75809d6feb5c1fbd36c7da29f1223d
Deepanshu Negi <deepanshu9760873328@gmail.com>
2025-01-30 01:12:46 +0530

    arrays

--- chaicode/array.js
+++ chaicode/array.js
@@ -1,5 +1,8 @@
 /* ARRAYS IN JAVASCRIPT */
 /* ARRAY IS A DATA STRUCTURE IN JAVASCRIPT */
+/* ARRAYS ARE DIFFERNT FROM OBJECTS OBJECTS ARE DECLARED IN CURLY BRACES 
+AND CONSIST KEY VALUE PAIR*/
+
 
 /* In JavaScript, a primitive 
 (primitive value, primitive data type) is data that is not an object and
@@ -15,6 +18,7 @@ null
 
 */
 
+
 /*
 JavaScript arrays are resizable and can contain a mix of different data types. (When those characteristics are undesirable, use typed arrays instead.)
 JavaScript arrays are not associative arrays and so, array elements cannot be accessed using arbitrary strings as indexes, but must be accessed using nonnegative integers (or their respective string form) as indexes.
@@ -27,5 +31,40 @@ const myarr = [0,"hekk" , 24,3533,4,34,true];
 console.log(myarr[3]);
 
 // arrays in js create shallow copies .
+myheros=myarr;
+// console.log(typeof(myheros)); // object
+
+// console.log(myheros[2]);
+
+// myheros.push("45"); // adding inside the
+// console.log(myheros);
+
+// console.log(typeof(myheros)); // object
+
+// console.log(myheros.pop()); // removes the last element of the array.
+// console.log(myheros);
+
+// myheros.unshift(78); // the unshift operator helps to add value to first position .
+// console.log(myheros);
+
+console.log(myheros.shift()); // removes and gives first element .
+ 
+
+// includes will help to check if the provided number is present in the array or not .
+console.log(myheros.includes(8)); 
 
 
+console.log(myheros.indexOf(8)); // -1
+
+
+const newarr = myarr.join();
+console.log(newarr); 
+console.log(typeof(newarr)); // string
+
+
+// SLICE AND SPLICE 
+
+console.log("A " , myarr);
+
+const hello = myarr.slice (1,3); // provides the value from two ends .
+console.log(hello);

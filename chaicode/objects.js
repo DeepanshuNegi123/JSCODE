a92b48effaad26e68fef1d5ff1ae965047557436
Deepanshu Negi <deepanshu9760873328@gmail.com>
2025-02-04 23:27:51 +0530

    half

--- chaicode/objects.js
+++ chaicode/objects.js
@@ -19,12 +19,12 @@ const user  = {
 //console.log(user['email']);  
 
 //console.log(user.checkn); // a string cannot be accessed through dot operator .
-console.log(user["check n"]); // through brackets .
+//console.log(user["check n"]); // through brackets .
 
 const mysymbol = Symbol();
 
 /// symbol is  a  unique immutable primitive datatype .
-console.log(mysymbol);
+//console.log(mysymbol);
 
 /// Symbol has s capital.
 
@@ -42,7 +42,7 @@ console.log();  // how to retrive
 
 
 user.email = "dkfd"; // we can change the users properties.
-console.log(user.email);
+//console.log(user.email);
 
 
 
@@ -53,17 +53,55 @@ console.log(user.email);
 
 //Object.freeze(user); // freeze method 
 user.name = "luffy kun";
-console.log(user.name ); // it didn't change
-console.log(user.age);
+//console.log(user.name ); // it didn't change
+//console.log(user.age);
 
 
 
 /*  --------------------------------------------------------------   FUNCTIONS IN JAVA SCRIPT +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
- // this will help us to access the properties in the user .
+ 
+// this will help us to access the properties in the user .
 user.greeting = function(){
-    console.log(`hello ${this.name} how are you i think you are fine `);
+    //console.log(`hello ${this.name} how are you i think you are fine `);
 }
 
-console.log(user.greeting); // function anonyms .
-console.log(user.greeting());
+// console.log(user.greeting); // function anonyms .
+// console.log(user.greeting());
+
+ const insta =  {
+
+ };
+// const insta = new object();
+
+console.log(insta);
+insta.name = "sammwer"; // 
+console.log(insta);
+
+
+// objects inside the obbjects 
+
+
+const regular = {
+  email : "luffy344",
+  name:"luffy",
+  id:"1232242",
+  subjects:{
+    1:"ai",
+    2:"maths",
+    3:"front end",
+    4:"backend "
+  }
+}
+
+
+// console.log(regular.subjects[3]);
+
+
+
+const obj1 = {1:"a" , 2:"b",3:"c"};
+const obj2 = {4:"d" ,5:"e" , 6:"f"};
+const obj3 = Object.assign({},obj1,obj2);
+console.log(obj3);
+
+

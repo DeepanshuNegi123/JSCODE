4a8f9a5a9fb68136d77f54e9dcd450294fe2115f
Deepanshu Negi <deepanshu9760873328@gmail.com>
2025-02-12 23:21:57 +0530

    loops

---
+++ chaicode/loops.js
@@ -0,0 +1,25 @@
+
+
+
+for (let index = 0; index<12; index++){
+    const element = index;
+    console.log(element);
+}
+
+
+for (let i = 0 ;  i <=10; i++){
+    // console.log(`outer loop value: ${i}`);
+
+for (let  j =0; j<=11; j++){
+    // console.log(`inner loop ${j}`);
+}
+}
+//break and continue 
+
+for (let a = 2; a<=20; a++){
+    console.log("value of a is " , a);
+    if (a == 7){
+      
+        continue;
+    }
+}

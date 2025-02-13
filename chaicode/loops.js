ae1a85b62203d1cd14e6e392bad3350da13b47c0
Deepanshu Negi <deepanshu9760873328@gmail.com>
2025-02-13 23:08:56 +0530

    while and do while

--- chaicode/loops.js
+++ chaicode/loops.js
@@ -1,25 +1,59 @@
 
 
 
-for (let index = 0; index<12; index++){
-    const element = index;
-    console.log(element);
-}
+// for (let index = 0; index<12; index++){
+//     const element = index;
+//     console.log(element);
+// }
 
 
-for (let i = 0 ;  i <=10; i++){
-    // console.log(`outer loop value: ${i}`);
+// for (let i = 0 ;  i <=10; i++){
+//     // console.log(`outer loop value: ${i}`);
 
-for (let  j =0; j<=11; j++){
-    // console.log(`inner loop ${j}`);
-}
-}
-//break and continue 
+// for (let  j =0; j<=11; j++){
+//     // console.log(`inner loop ${j}`);
+// }
+// }
+// //break and continue 
 
-for (let a = 2; a<=20; a++){
-    console.log("value of a is " , a);
-    if (a == 7){
+// for (let a = 2; a<=20; a++){
+//     console.log("value of a is " , a);
+//     if (a == 7){
       
-        continue;
-    }
+//         continue;
+//     }
+// }
+
+
+// do while and while 
+
+let index = 0;
+
+while (index<=22){
+console.log("yes");
+console.log(`this  is ${index}`);
+index++;
 }
+
+
+let array = ['hello' , 'world' , 'iam ' , 'cryptic'];
+
+let arr = 0;
+while(arr<array.length){
+console.log(array[arr]);
+arr++;
+
+}
+
+
+
+let i =0;
+do {
+
+
+console.log("underworld");
+i++;
+
+}
+
+while(i<20);

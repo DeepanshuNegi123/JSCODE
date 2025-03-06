0c1987818bbd65d92fcf2be69df51e8ffb19919b
Deepanshu Negi <deepanshu9760873328@gmail.com>
2025-03-06 23:28:25 +0530

    this comments

--- projects/colorchange/color.js
+++ projects/colorchange/color.js
@@ -1,11 +1,11 @@
 // const hello = document.querySelector('.this input');
 // console.log(hello);
 
-const heha =  document.querySelectorAll('.button');
+const heha =  document.querySelectorAll('.this'); // selecting .this class and storing in heha heha is a nodelist having similar functions as an array.
 console.log(heha);
 
 
-heha.forEach((hehe) => 
+heha.forEach((hehe) =>  // applying foreach to the heha for accesing the values in it .
 
     {
         // console.log(hehe);
@@ -14,11 +14,11 @@ heha.forEach((hehe) =>
         hehe.addEventListener('click',function(hello){
          
             // console.log(hello);
-            alert(`button clicked  is ${hello.target.id}`);
+            alert(`button clicked  is ${hello.target.id}`); // when button clicked we will get alert on screen displaying id of the targeted element .
             console.log(hello.target); // helps us in getting the 
             console.log(hello);
             const color= hello.target.id;
-            document.body.style.backgroundColor=color;
+            document.body.style.backgroundColor=color; // changing  the background color as per the selected button clicked .
             
         });
 

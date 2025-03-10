e81f71a34e9c5dc56bbe6dc10ae9c2fbb22ffede
Deepanshu Negi <deepanshu9760873328@gmail.com>
2025-03-10 15:10:59 +0530

    this is commit

--- chaicode/events/event.js
+++ chaicode/events/event.js
@@ -9,9 +9,62 @@
 //     alert('this is last image');
 // }
 
-document.getElementById('prayer0').addEventListener('click',(e)=>{
-    alert('image is clicked ');
-    console.log(e.timeStamp); // gives  us timestamp of the clicked images .
-    
 
-})
+
+
+ // ++++++++++++++++++++++++++++  EVENT PROPOGATION  +++++++++++++++++++++++++++++++++++++++//
+ // HERE IN EVENT PROPOGATION WE GET TO KNOW ABOUT WAYS 
+ // OF ACCESSING THE EVENT EITHER DIRECT TARGET OR FROM ROOT .
+ // PARAMETER  THIRD OF EVENTLISTNER HELPS IN FINDING OUT  PROPOGATION .
+
+
+ /*
+
+
+document.getElementById('images').addEventListener('click',(e)=>{
+    alert('images ul  is clicked ');
+    console.log("images ul  is clicked");   
+
+},true);  // HERE WE WILL GET FIRST IMAGES TAG OR THIS ONE 
+
+document.getElementById('prayer1').addEventListener('click' , (event)=>{
+console.log("clicked inside the ul");
+alert('clicked inside the ul');
+},true);   // ONCLICKING PRAYER1 ALSO WE WILL GET ALERT OF IMAGES .
+
+
+document.getElementById('images').addEventListener('click',(e)=>{
+    alert('images ul  is clicked ');
+    console.log("images ul  is clicked");   
+
+});
+
+document.getElementById('prayer1').addEventListener('click' , (event)=>{
+console.log("clicked inside the ul");
+alert('clicked inside the ul'); // THIS WILL SHOW FIRST ON FIRST CLICKING THIS THEN SECONDLY IMAGES WILL BE SHOWED.
+event.stopPropagation(); // this will end the bubbling at target only no going back to root .
+
+});
+
+document.getElementById('google').addEventListener('click',(e)=>{
+e.preventDefault(); // link will not open on clicking 
+e.stopImmediatePropagation();
+},false ) ;
+
+*/
+
+document.querySelector('#images').addEventListener('click',(event)=>{
+//    console.log(event.target);
+   console.log(event.target.parentNode); // li 
+  let remove = event.target.parentNode;
+  if (event.target.tagName === 'IMG'){
+    console.log(event.target.id);
+    let remove = event.target.parentNode;
+    remove.remove();
+
+  }
+  console.log(event.target.tagName); // like img 
+ 
+//   remove.remove(); // helps in removing the whole parent node .
+
+},false)

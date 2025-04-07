63847812f0bf14146ece8bab9da04579ee469cbc
DeepanshuNegi123 <deepanshu9760873328@gmail.com>
2025-04-07 23:50:15 +0530

    promise.js

---
+++ API/promise.js
@@ -0,0 +1,18 @@
+// the promise object represents  us 
+// the eventual completion of 
+// the asynchronous operation and its value.
+
+
+// WORKING OF API AND 
+
+// APIs often return Promises in JavaScript 
+// because API calls are asynchronous 
+// (they take time to respond).
+// Promises help manage APIs (and other async tasks).
+
+// states of promise 
+// pending 
+// fulfilled 
+// rejected 
+
+

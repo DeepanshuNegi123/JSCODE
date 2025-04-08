ddb344da74199c05b495bb31419ce715e7b572d9
DeepanshuNegi123 <deepanshu9760873328@gmail.com>
2025-04-09 00:26:09 +0530

    promise

--- API/promise.js
+++ API/promise.js
@@ -16,3 +16,14 @@
 // rejected 
 
 
+const promiseone= new Promise (function (resolve, reject){
+
+    setTimeout(function (){
+        console.log('Async task is complete');
+        resolve();
+    },3000)
+})
+
+promiseone.then(function (){
+    console.log("promise consumed");
+}); // resolve is   connected to  .then

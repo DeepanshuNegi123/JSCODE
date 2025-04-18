aa0879bb8437d79fe3c35b903f4c5b0d2fd66b56
DeepanshuNegi123 <deepanshu9760873328@gmail.com>
2025-04-18 23:20:57 +0530

    fetching through get and post

--- API/fetch.js
+++ API/fetch.js
@@ -4,6 +4,42 @@
 
 // docs - https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 
-fetch('https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch')
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts',{
+//     method: 'post',
+//     body : JSON.stringify({
+//         posted:2,
+//         name:'hello',
+//         email:"cryptic@gmail.com",
+//         body:'this is awesom!'
+//     })
+// })
+// .then(response => response.json()).then(find => console.log(find.text()))
+// .then(data => console.log(data)). catch(error => console.log("error fouund"));
+
+
+
+fetch('https://jsonplaceholder.typicode.com/comments/1', {
+     
+    method:'GET',  // need not to be specified default get is method .
+
+
+}).then(response => console.log(response.text())) 
+.then(data => console.log(data));
+
+
+
+fetch('https://jsonplaceholder.typicode.com/comments',{
+
+    method:'POST',
+    headers:
+    {
+      'Content-Type':'application/json'
+
+    },
+
+    body: JSON.stringify( {body :"hello guys how are you all"})
+})
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => console.log(data));
